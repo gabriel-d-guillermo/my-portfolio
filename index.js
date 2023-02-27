5c809a282890dc2cd02392c838af7d6c64e75bf2
gabriel-d-guillermo <109959568+gabriel-d-guillermo@users.noreply.github.com>
2023-02-28 02:09:07 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function toggleDarkMode() {
+  let element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+function toogleNavBar() {
+  const navBar = document.querySelector(".nav-wrapper");
+  navBar.classList.toggle("navbar-expand");
+}
