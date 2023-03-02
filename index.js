43b4a3eb48f2551bfe243e956d88d2f94fd3c31b
gabriel-d-guillermo <109959568+gabriel-d-guillermo@users.noreply.github.com>
2023-03-03 02:35:07 +0800

    add dark mode

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
+const body = document.body;
+
+window.onload = function () {
+  body.style.opacity = 1;
+};
+
+const element = document.querySelector(".fade-in");
+const slider = document.querySelector(".slider");
+const iconMoon = document.querySelector(".fa-moon");
+const iconSun = document.querySelector(".fa-sun");
+
+window.addEventListener("scroll", function () {
+  // var position = element.getBoundingClientRect();
+  // Get the current scroll position
+  const scrollPosition = window.scrollY;
+  // Calculate the opacity based on the scroll position
+  const opacity = scrollPosition / 800;
+
+  // console.log(opacity - scrollPosition);
+
+  if (opacity >= 0) {
+    element.style.opacity = opacity;
+  }
+});
+
 function toggleDarkMode() {
-  let element = document.body;
-  element.classList.toggle("dark-mode");
+  body.classList.toggle("dark-mode");
+  slider.classList.toggle("toogle");
+  iconMoon.classList.toggle("icon-opacity-moon");
+  iconSun.classList.toggle("icon-opacity-sun");
+
+  console.log(slider);
 }
 
 function toogleNavBar() {
-  const navBar = document.querySelector(".nav-wrapper");
+  const navBar = document.querySelector("nav");
   navBar.classList.toggle("navbar-expand");
+  body.classList.toggle("over-flow");
 }
